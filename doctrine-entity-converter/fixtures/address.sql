784f5a3ace88004de7479a845ad399448eadf4d4
pjordaan <pjordaan@users.noreply.github.com>
2022-09-07 18:42:50 +0000

    Update file(s) "packages/doctrine-entity-converter" from "apie-lib/apie-lib-monorepo"

--- doctrine-entity-converter/fixtures/address.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO UserWithAutoincrementKey(id, password_serializedString, password_originalClass, address_serializedString, address_originalClass)
-VALUES (42, 'N;', 'null', 'N;', 'string');
