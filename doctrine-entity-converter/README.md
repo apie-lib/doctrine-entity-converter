fceebb78387f138ef5671156982ae48ae2238bff
pjordaan <pjordaan@users.noreply.github.com>
2022-09-07 18:45:59 +0000

    Update file(s) "packages/doctrine-entity-converter" from "apie-lib/apie-lib-monorepo"

---
+++ doctrine-entity-converter/README.md
@@ -0,0 +1,17 @@
+<img src="https://raw.githubusercontent.com/apie-lib/apie-lib-monorepo/main/docs/apie-logo.svg" width="100px" align="left" />
+<h1>doctrine-entity-converter</h1>
+
+
+
+
+
+
+ [![Latest Stable Version](http://poser.pugx.org/apie/doctrine-entity-converter/v)](https://packagist.org/packages/apie/doctrine-entity-converter) [![Total Downloads](http://poser.pugx.org/apie/doctrine-entity-converter/downloads)](https://packagist.org/packages/apie/doctrine-entity-converter) [![Latest Unstable Version](http://poser.pugx.org/apie/doctrine-entity-converter/v/unstable)](https://packagist.org/packages/apie/doctrine-entity-converter) [![License](http://poser.pugx.org/apie/doctrine-entity-converter/license)](https://packagist.org/packages/apie/doctrine-entity-converter) [![PHP Version Require](http://poser.pugx.org/apie/doctrine-entity-converter/require/php)](https://packagist.org/packages/apie/doctrine-entity-converter) [![Code coverage](https://raw.githubusercontent.com/apie-lib/doctrine-entity-converter/main/coverage_badge.svg)](https://apie-lib.github.io/coverage/doctrine-entity-converter/index.html)  
+
+[![PHP Composer](https://github.com/apie-lib/doctrine-entity-converter/actions/workflows/php.yml/badge.svg?event=push)](https://github.com/apie-lib/doctrine-entity-converter/actions/workflows/php.yml)
+
+This package is part of the [Apie](https://github.com/apie-lib) library.
+The code is maintained in a monorepo, so PR's need to be sent to the [monorepo](https://github.com/apie-lib/apie-lib-monorepo/pulls)
+
+## Documentation
+This package is used internally in Apie or no documentation is available right now
---
+++ doctrine-entity-converter/src/PropertyGenerators/UuidGenerator.php
@@ -0,0 +1,82 @@
+<?php
+namespace Apie\DoctrineEntityConverter\PropertyGenerators;
+
+use Apie\Core\Identifiers\Uuid;
+use Apie\Core\ValueObjects\Utils;
+use Apie\DoctrineEntityConverter\Interfaces\PropertyGeneratorInterface;
+use Apie\DoctrineEntityConverter\Mediators\GeneratedCode;
+use Apie\DoctrineEntityConverter\Utils\Utils as UtilsUtils;
+use Doctrine\ORM\Mapping\Column;
+use ReflectionClass;
+use ReflectionNamedType;
+use ReflectionProperty;
+
+/**
+ * @template T of object
+ * @implements PropertyGeneratorInterface<T>
+ */
+class UuidGenerator implements PropertyGeneratorInterface
+{
+    public function isSupported(ReflectionClass $class, ReflectionProperty $property): bool
+    {
+        $type = $property->getType();
+        if (!($type instanceof ReflectionNamedType)) {
+            return false;
+        }
+        if ($type->isBuiltin()) {
+            return false;
+        }
+        return class_exists($type->getName())
+            && ($type->getName() === Uuid::class || is_a($type->getName(), Uuid::class, true));
+    }
+
+    public function apply(GeneratedCode $code, ReflectionClass $class, ReflectionProperty $property): void
+    {
+        $fromCode = $this->generateFromCode($class, $property);
+        $code->addCreateFromCode($fromCode);
+        $inject = $this->generateInject($class, $property);
+        $code->addInjectCode($inject);
+        $prop = $code->addProperty('string', $property->name)
+            ->addAttribute(Column::class, ['type' => 'string', 'length' => 36]);
+        UtilsUtils::addIdAttributeIfApplicable($class->name, $property->getType(), $prop);
+    }
+
+    /**
+     * @param ReflectionClass<T> $class
+     */
+    protected function generateFromCode(ReflectionClass $class, ReflectionProperty $property): string
+    {
+        $declaringClass = 'OriginalDomainObject';
+        if ($property->getDeclaringClass()->name !== $class->name) {
+            $declaringClass = '\\' . $property->getDeclaringClass()->name;
+        }
+        return sprintf(
+            '$tmp = Utils::getProperty($input, new \ReflectionProperty(%s::class, %s));
+$tmp = $tmp ? $tmp->toNative(): null;
+$instance->%s = $tmp === null ? null : \%s::toString($tmp);',
+            $declaringClass,
+            var_export($property->name, true),
+            $property->name,
+            Utils::class
+        );
+    }
+
+    /**
+     * @param ReflectionClass<T> $class
+     */
+    protected function generateInject(ReflectionClass $class, ReflectionProperty $property): string
+    {
+        $declaringClass = 'OriginalDomainObject';
+        if ($property->getDeclaringClass()->name !== $class->name) {
+            $declaringClass = '\\' . $property->getDeclaringClass()->name;
+        }
+        $propertyClass = '\\' . $property->getType();
+        return sprintf(
+            'Utils::setProperty($instance, new \ReflectionProperty(%s::class, %s), %s::fromNative($this->%s));',
+            $declaringClass,
+            var_export($property->name, true),
+            $propertyClass,
+            $property->name
+        );
+    }
+}
