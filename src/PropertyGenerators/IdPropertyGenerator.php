00ced1aa3f5e2f2586e90ad10ae4f92e1174214e
pjordaan <pjordaan@users.noreply.github.com>
2023-10-29 14:42:29 +0000

    Update file(s) "packages/doctrine-entity-converter/." from "apie-lib/apie-lib-monorepo"

--- src/PropertyGenerators/IdPropertyGenerator.php
+++ src/PropertyGenerators/IdPropertyGenerator.php
@@ -1,14 +1,17 @@
 <?php
 namespace Apie\DoctrineEntityConverter\PropertyGenerators;
 
+use Apie\Core\Entities\PolymorphicEntityInterface;
 use Apie\Core\Persistence\Fields\EntityGetIdValue;
 use Apie\Core\Persistence\PersistenceFieldInterface;
 use Apie\Core\Persistence\PersistenceTableInterface;
+use Apie\Core\Utils\EntityUtils;
 use Apie\DoctrineEntityConverter\Interfaces\PropertyGeneratorInterface;
 use Apie\DoctrineEntityConverter\Mediators\GeneratedCode;
 use Doctrine\ORM\Mapping\Column;
 use Doctrine\ORM\Mapping\GeneratedValue;
 use Doctrine\ORM\Mapping\Id;
+use ReflectionClass;
 
 final class IdPropertyGenerator implements PropertyGeneratorInterface
 {
@@ -20,6 +23,13 @@ final class IdPropertyGenerator implements PropertyGeneratorInterface
     public function apply(GeneratedCode $code, PersistenceTableInterface $table, PersistenceFieldInterface $field): void
     {
         $fromCode = $this->generateFromCode($table, $field);
+        $originalClass = $table->getOriginalClass();
+        if ($originalClass) {
+            $refl = new ReflectionClass($originalClass);
+            if ($refl->implementsInterface(PolymorphicEntityInterface::class)) {
+                $fromCode .= PHP_EOL . $this->generatePolymorphicCode($table, $field);
+            }
+        }
         $code->addCreateFromCode($fromCode);
         $prop = $code->addProperty(($field->isAllowsNull() ? '?' : '') . $field->getPersistenceType()->toType(), $field->getName());
         $prop->addAttribute(
@@ -33,6 +43,14 @@ final class IdPropertyGenerator implements PropertyGeneratorInterface
         if ($field->isAllowsNull()) {
             $prop->addAttribute(GeneratedValue::class, ['strategy' => 'IDENTITY']);
         }
+    }
+
+    protected function generatePolymorphicCode(PersistenceTableInterface $table, PersistenceFieldInterface $field): string
+    {
+        return '$instance->discriminatorMapping = \\' . EntityUtils::class . '::getDiscriminatorValues(
+    $input,
+    new \ReflectionClass(self::getOriginalClassName())
+);';
     }
 
     protected function generateFromCode(PersistenceTableInterface $table, PersistenceFieldInterface $field): string
