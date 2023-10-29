445303df4ff66fb22d8478f4fe75c06f8f9f328b
pjordaan <pjordaan@users.noreply.github.com>
2023-10-29 13:48:28 +0000

    Update file(s) "packages/doctrine-entity-converter/." from "apie-lib/apie-lib-monorepo"

--- src/Concerns/IsLinkedToEntity.php
+++ src/Concerns/IsLinkedToEntity.php
@@ -1,6 +1,14 @@
 <?php
 namespace Apie\DoctrineEntityConverter\Concerns;
 
+use Apie\Core\Entities\EntityInterface;
+use ReflectionClass;
+
 trait IsLinkedToEntity
 {
+    public function newDomainClassInstance(): EntityInterface
+    {
+        $refl = new ReflectionClass($this->getOriginalClassName());
+        return $refl->newInstanceWithoutConstructor();
+    }
 }
